6deb3b6872e522d9a73c7309a948ada8df7ee297
Stacey <stacey.mulcahy@gmail.com>
2013-06-19 14:44:56 -0400

    initial commit
    Basic Instagram API integration for windows 8. Does not include oauth,
    only userless endpoints. Reusable classes for basic instagram & js - see
    js/instagram.js and apiglobals.js.

---
+++ APIMASH_INSTAGRAM_API/APIMASH_INSTAGRAM_API/js/apiglobals.js
@@ -0,0 +1,12 @@
+﻿
+var InstagramAPIGlobals = function () {
+
+    // this will be a private property
+    var clientId = '4e489408cfc84a9a99d125fbc7350945';
+  
+    return {
+        // declare which properties and methods are supposed to be public
+        clientId:clientId
+
+    }
+}();
