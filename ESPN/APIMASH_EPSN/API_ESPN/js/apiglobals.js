8939369bcba22ee606faae489f94ff7ff141f106
Stacey <stacey.mulcahy@gmail.com>
2013-06-24 17:52:31 -0400

    Adding espn
    starter windows 8 kit for espn. this is an espn api explorer. Requires
    an app id/key

--- ESPN/APIMASH_EPSN/API_ESPN/js/apiglobals.js
+++ ESPN/APIMASH_EPSN/API_ESPN/js/apiglobals.js
@@ -1,12 +1,15 @@
 ﻿
-var InstagramAPIGlobals = function () {
+var ESPNAPIGlobals = function () {
 
     // this will be a private property
-    var clientId = 'cfabed4191d748b9a64dc320fc80f92a';
+    var apiKey = 'gqkr6dk8gx2taqedra68j567';
   
+   
+
     return {
         // declare which properties and methods are supposed to be public
-        clientId:clientId
+        apiKey: apiKey,
+   
 
     }
 }();
