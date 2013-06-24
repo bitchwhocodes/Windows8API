b6015c981d0eb66cbd60e5a3fe45fff76ff029b5
Stacey <stacey.mulcahy@gmail.com>
2013-06-24 18:01:41 -0400

    Removed app key
    Removed app key

--- ESPN/APIMASH_EPSN/API_ESPN/js/apiglobals.js
+++ ESPN/APIMASH_EPSN/API_ESPN/js/apiglobals.js
@@ -1,15 +1,7 @@
 ﻿
 var ESPNAPIGlobals = function () {
-
-    // this will be a private property
-    var apiKey = 'gqkr6dk8gx2taqedra68j567';
-  
-   
-
+    var apiKey = '[APPLICATION-KEY]';
     return {
-        // declare which properties and methods are supposed to be public
         apiKey: apiKey,
-   
-
     }
 }();
