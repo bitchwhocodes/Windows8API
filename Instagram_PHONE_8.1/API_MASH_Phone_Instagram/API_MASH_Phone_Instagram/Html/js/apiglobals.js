319758085dfde7e97b5da5f4b1a9d5ae5b132638
Stacey <stacey.mulcahy@gmail.com>
2013-07-29 13:40:55 -0400

    New Instagram Phone
    This is 8.1 and needs to be tagged accordingly

---
+++ Instagram_PHONE_8.1/API_MASH_Phone_Instagram/API_MASH_Phone_Instagram/Html/js/apiglobals.js
@@ -0,0 +1,12 @@
+﻿
+var InstagramAPIGlobals = function () {
+
+    // this will be a private property
+    var clientId = 'cfabed4191d748b9a64dc320fc80f92a';
+  
+    return {
+        // declare which properties and methods are supposed to be public
+        clientId:clientId
+
+    }
+}();
---
+++ Instagram_PHONE_8.1/API_MASH_Phone_Instagram/API_MASH_Phone_Instagram/My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports System.Resources
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("API_MASH_Phone_Instagram")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyConfiguration("")> 
+<Assembly: AssemblyCompany("Microsoft Corp.")> 
+<Assembly: AssemblyProduct("API_MASH_Phone_Instagram")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft Corp. 2010")> 
+<Assembly: AssemblyTrademark("")> 
+<Assembly: AssemblyCulture("")> 
+
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<assembly: ComVisible(false)>
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<assembly: Guid("fc34516c-49ac-488e-bf78-52d4368243c0")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Revision and Build Numbers 
+' by using the '*' as shown below:
+<assembly: AssemblyVersion("1.0.0.0")>
+<assembly: AssemblyFileVersion("1.0.0.0")>
+<assembly: NeutralResourcesLanguageAttribute("en-US")>
